3149ae263136e3e8e3a1c1bc97398e2d5b3cf3c0
louis <louis.hocquet@sfr.fr>
2024-10-01 20:09:58 +0200

    Finished validation rules for form

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :registrations
+
+  def label
+    "#{name} (#{year_min}-#{year_max})"
+  end
 end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,4 +2,23 @@ class Registration < ApplicationRecord
   has_and_belongs_to_many :categories
   attr_accessor :year_min, :year_max, :name
   accepts_nested_attributes_for :categories
+
+  validates :category_ids, :presence => { message: :one_category_min }
+
+  validates :firstname, :presence => { message: :no_blank }
+  validates :lastname, :presence => { message: :no_blank }
+  validates :club_name, :presence => { message: :no_blank }
+  validates :country, :presence => { message: :no_blank }
+  validates :email,
+  :presence => { message: :no_blank },
+    :format => {
+      :with => /\S+@\S+\.\S+/i,
+      :message => :invalid_email
+    }
+  validates :phone,
+  :presence => { message: :no_blank },
+    :format => {
+      :with => /\A[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}\z/i,
+      :message => :invalid_phone
+    }
 end
